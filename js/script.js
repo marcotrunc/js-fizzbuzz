97c48ae37e307635b76ae080e3cf5b904d018b93
marcotrunc <marcotrunc@gmail.com>
2021-12-22 14:18:35 +0100

    created js-fizzbuzz repo

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+BONUS:
+Provare a stampare la lista in pagina invece che in console.
+*/
+// 1- Recuperare elemento in pagina
+// 2- Dichiarare una variabile di appoggio
+// 3- Ciclo for
+// 4- Primo if per determinare i numeri che sono divisibili per 3
+// 5- Secondo if per determinare i numeri che sono divisibili per 5
+// 6- Stampare la varibile in base alle condizioni 
+
+//** Recupero elemento in pagina
+const listElement = document.getElementById('list-element');
+
+//** Dichiaro una variabile di appoggio
+number
