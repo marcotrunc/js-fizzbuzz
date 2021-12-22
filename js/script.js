55c53f94fdbadb871c578ff6bd045e806dc48149
marcotrunc <marcotrunc@gmail.com>
2021-12-22 15:26:52 +0100

    added the contition that i is a multiple of three and five

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 console.log('JS OK!')
 /*
 Scrivi un programma che stampi i numeri da 1 a 100,
-ma per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
+ma per i multipli di 3 stampi Fizz al posto del numero e per i multipli 
+di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 BONUS:
@@ -23,15 +24,18 @@ let listNumber = '';
 //** Ciclo for
 for(let i = 1; i <= 100; i++){
     //** Ciclo if per vedere se il numero è divisibile per 3
-    if(i % 3 === 0){
+    if(i % 3 === 0 && i % 5 === 0){
+    listNumber += `fizzBuzz`
+    }
+    else if(i % 3 === 0){
         listNumber += `fizz `;
     
      //** Ciclo if per vedere se il numero è divisibile per 3
     }else if (i % 5 === 0){
-        listNumber += `fizzBuzz `;
+        listNumber += `buzz `;
         
     }else{
-        listNumber += `${i} `;
+    listNumber += `${i} `;
     }
 }
 
